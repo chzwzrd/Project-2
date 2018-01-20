4b13f5048b8d68579614337e55d599e81b7718d4
Sumaira Memon <sumairamemon@dhcp-v096-075.mobile.uci.edu>
2018-01-20 14:01:49 -0800

    updated

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 // *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// api-routes.js - this file offers a set of routes for displaying and saving data to themodels
 // *********************************************************************************
 
 // Dependencies
@@ -12,83 +12,83 @@ var models = require("../models");
 // =============================================================
  // GET route for getting all user
  app.get("/api/user/", function(req, res) {
-    db.user.findAll({})
-    .then(function(dbuser) {
-      res.json(dbuser);
+   models.UserInfo.findAll({})
+    .then(function(dbUserInfo) {
+      res.json(dbUserInfo);
     });
   });
 
-  // Get route for returning user
-  app.get("/api/user/category/:category", function(req, res) {
-    db.user.findAll({
+  // Get route for returning UserInfo
+  app.get("/api/UserInfo/category/:category", function(req, res) {
+   models.UserInfo.findAll({
       Order: {
         category: req.params.category
       }
     })
-    .then(function(dbuser) {
-      res.json(dbuser);
+    .then(function(dbUserInfo) {
+      res.json(dbUserInfo);
     });
   });
 
-  // Get rotue for retrieving a single user
-  app.get("/api/user/:id", function(req, res) {
-    db.user.findOne({
+  // Get rotue for retrieving a single UserInfo
+  app.get("/api/UserInfo/:id", function(req, res) {
+   models.UserInfo.findOne({
       where: {
-        name: req.params.name
-        email: req.params.email
-        phone: req.params.phone
-        zip: req.params.zip
+        name: req.params.name,
+        email: req.params.email,
+        phone: req.params.phone,
+        zip: req.params.zip,
       }
     })
-    .then(function(dbuser) {
-      res.json(dbuser);
+    .then(function(dbUserInfo) {
+      res.json(dbUserInfo);
     });
   });
 
-  // user route for saving a new user
-  app.post("/api/user", function(req, res) {
+  // UserInfo route for saving a new UserInfo
+  app.post("/api/UserInfo", function(req, res) {
     console.log(req.body);
-    db.user.create({
+   models.UserInfo.create({
       name: req.body.name,
       email: req.body.email,
-      phone: req.body.phone
-      zip: req.body.zip
+      phone: req.body.phone,
+      zip: req.body.zip,
     
     })
-    .then(function(dbuser) {
-      res.json(dbuser);
+    .then(function(dbUserInfo) {
+      res.json(dbUserInfo);
     });
   });
 
-  // DELETE route for deleting user
-  app.delete("/api/user", function(req, res) {
-    db.user.destroy({
+  // DELETE route for deleting UserInfo
+  app.delete("/api/UserInfo", function(req, res) {
+   models.UserInfo.destroy({
       where: {
-      name: req.params.name
-      email: req.params.email
-     phone: req.params.phone
-     zip: req.params.zip
+      name: req.params.name,
+      email: req.params.email,
+     phone: req.params.phone,
+     zip: req.params.zip,
       }
     })
-    .then(function(dbuser) {
-      res.json(dbuser);
+    .then(function(dbUserInfo) {
+      res.json(dbUserInfo);
     });
   });
 
   // PUT route for Rent Histroy
   app.put("/api/renthistory", function(req, res) {
-    db.user.update(req.body,
+   models.UserInfo.update(req.body,
       {
         where: {
-        OrderID:: req.body.OrderID
-          petname: req.body.petname
-          gender: req.body.gender
-          date: req.body.date
-          location: req.body.location
+        OrderID:: req.body.OrderID,
+          petname: req.body.petname,
+          gender: req.body.gender,
+          date: req.body.date,
+          location: req.body.location,
         }
       })
-    .then(function(dbuser) {
-      res.json(dbuser);
+    .then(function(dbUserInfo) {
+      res.json(dbUserInfo);
     });
   });
 };
