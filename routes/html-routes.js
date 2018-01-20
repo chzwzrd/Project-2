669228ee61030d7bfbfb1582ec3d900a0c3e5a52
Aline Espino <alin.e.spino@gmail.com>
2018-01-20 13:54:52 -0800

    using router and req in server

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,39 +1,39 @@
 
 
 var path = require("path");
+var express = require("express");
+router = express.Router();
+
 
 // Handlebars Routes //
 // Handles the HTML page that the user gets sent to //
 
-module.exports = function(app) {
-
-    app.get("/", function (req, res) {
-        res.render("index",hbsObject);
-        console.log(hbsObject);
+    router.get("/", function (req, res) {
+        res.render("index");
     });
 
-    app.get("/search", function (req, res) {
-        res.render("search",hbsObject);
+    router.get("/search", function (req, res) {
+        res.render("search");
     });
 
-    app.get("/results", function (req, res) {
-        res.render("results",hbsObject);
+    router.get("/results", function (req, res) {
+        res.render("results");
     });
 
-    app.get("/rent", function (req, res) {
-        res.render("rent",hbsObject);
+    router.get("/rent", function (req, res) {
+        res.render("rent");
     });
 
-    // app.get("/favorites", function (req, res) {
+    router.get("/rented", function (req, res) {
+    res.render("rented");
+    });
+
+// Define a hbsobject and then pass it as response in res.render //
+    // router.get("/favorites", function (req, res) {
     //     res.render("favorites",hbsObject);
     // });
 
-    // app.get("/history", function (req, res) {
+    // router.get("/history", function (req, res) {
     //     res.render("history",hbsObject);
     // });
-
-    // app.get("/rented", function (req, res) {
-    //     res.render("rented",hbsObject);
-    // });
-
-};
+    module.exports = router;
