e228eef32b48c8caf6d2f1b4ab0ee9681614e69f
Melodie Chi <chzwzrd@dhcp-v024-237.mobile.uci.edu>
2018-01-21 14:12:32 -0800

    fixed hbs_controller.js request URL, commented out renthistory.js, fixed search.handlebars select options

--- controllers/hbs_controller.js
+++ controllers/hbs_controller.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const petFinderURL = 'http://api.petfinder.com/pet.get?key=19d36f366ea3a2b37ba86aaeb7a5bbea&id=15860233&format=json';
+const petFinderURL = 'http://api.petfinder.com/pet.get?key=19d36f366ea3a2b37ba86aaeb7a5bbea&format=json';
 // on frontend when submit hit, they are routed to this url
-router.get("/pets&:age&:breed&:size&:breed&:location&:gender", (req, res) => {
-    axios.get(petFinderURL+ `&animal=dog&age=${req.params.age}
-    &breed=${req.params.breed}&gender=${req.params.gender}
-    &location=${req.params.location}&size=${req.params.size}`)
+router.get("/pets&:animal&:breed&:age&:sex&:location", (req, res) => {
+    axios.get(petFinderURL + `&animal=${req.params.animal}&breed=${req.params.breed}&age=${req.params.age}
+    &sex=${req.params.sex}&location=${req.params.zipcode}`)
     .then(function (response) {
         console.log(response);
-         
     })
     .catch(function (error) {
         console.log(error);
--- models/renthistory.js
+++ models/renthistory.js
@@ -1,51 +1,51 @@
-// Going to need models for:
+// // Going to need models for:
 
-// 1. User (POST)
-// 2. Dogs (GET)
-// 3. RentHistory (FOREIGN KEYS: User and Dog) 
-//    use handlebars for this, axios here, res.render, save to an
+// // 1. User (POST)
+// // 2. Dogs (GET)
+// // 3. RentHistory (FOREIGN KEYS: User and Dog) 
+// //    use handlebars for this, axios here, res.render, save to an
 
-// db stuff in api files
+// // db stuff in api files
 
-// Sequelize (capital) references the standard library
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/connection.js");
+// // Sequelize (capital) references the standard library
+// var Sequelize = require("sequelize");
+// // sequelize (lowercase) references my connection to the DB.
+// var sequelize = require("../config/connection.js");
 
-// ___ START of RentHistory model   __________________________________________________________________________________________________________________________
-module.exports = function (sequelize, DataTypes) {
-    // Defining our User table
-    var RentHistory = sequelize.define("UserHistory", {
-        OrderID: {
-            type: Sequelize.INTEGER
-        },
-        PetName: {
-            type: Sequelize.STRING
-        },
-        Gender: {
-            type: Sequelize.STRING
-        },
-        DATE: {
-            type: Sequelize.INTEGER
-        },
-        Location: {
-            type: Sequelize.STRING
-        }
-    });
+// // ___ START of RentHistory model   __________________________________________________________________________________________________________________________
+// module.exports = function (sequelize, DataTypes) {
+//     // Defining our User table
+//     var RentHistory = sequelize.define("UserHistory", {
+//         OrderID: {
+//             type: Sequelize.INTEGER
+//         },
+//         PetName: {
+//             type: Sequelize.STRING
+//         },
+//         Gender: {
+//             type: Sequelize.STRING
+//         },
+//         DATE: {
+//             type: Sequelize.INTEGER
+//         },
+//         Location: {
+//             type: Sequelize.STRING
+//         }
+//     });
 
-    RentHistory.associate = function (models) {
+//     RentHistory.associate = function (models) {
 
-        // Setting a many to one relationship 
-        RentHistory.belongsToMany(models.User, {
+//         // Setting a many to one relationship 
+//         RentHistory.belongsToMany(models.User, {
 
-        });
-        //
-    };
+//         });
+//         //
+//     };
     
-    return RentHistory;
-};
+//     return RentHistory;
+// };
 
-// ___ END of RentHistory Model   _____________________________________________________________________________________________________________________________
+// // ___ END of RentHistory Model   _____________________________________________________________________________________________________________________________
 
 
 
