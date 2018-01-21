96d170eb84472e27080e00ed7b00f7ae10012980
John <jprickett92@gmail.com>
2018-01-21 15:25:34 -0800

    troubleshooting. commented out a lot of stuff. commiting in case we need to go back and look at my debugging. otherwise will use melodie's moving forward.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require ("method-override");
+var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 var sequelize = require("sequelize");
 
@@ -16,7 +16,8 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Handlebars routes for pet search controller
-var hbsRoutes = require("./controllers/hbs_controller");
+// John added .js to hbs_controller in below route
+var hbsRoutes = require("./controllers/hbs_controller.js");
 app.use("/", hbsRoutes);
 
 // Data parsing for Express app //
@@ -37,20 +38,19 @@ app.set("view engine", "handlebars");
 
 // Routes // 
 var htmlRoutes = require("./routes/html-routes.js");
-app.use("/",routes);
+app.use("/", "routes");
 
 // Sync sequelize models and start Express app //
 
-models.sequelize.sync({ force: true }).then(function() {
-    //title and column name have to be the same for bulk insert
-    models.UserInfo.bulkCreate (
-      {name: "John", email: "jprickett92@gmail.com", phone: "9496982525", zip: "92663"},
-      {name: "Aline", email: "xyz@gmail.com", phone: "949555555", zip: "92660"},
-      {name: "Mel", email: "abc@gmail.com", phone: "9498675309", zip: "90210"},
-      {name: "Sumaira", email: "sum@gmail.com", phone: "7142222222", zip: "90001"}
-    )
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
-    });
-  });
-  
+// models.sequelize.sync({ force: true }).then(function() {
+//     //title and column name have to be the same for bulk insert
+//     models.UserInfo.bulkCreate (
+//       {name: "John", email: "jprickett92@gmail.com", phone: "9496982525", zip: "92663"},
+//       {name: "Aline", email: "xyz@gmail.com", phone: "949555555", zip: "92660"},
+//       {name: "Mel", email: "abc@gmail.com", phone: "9498675309", zip: "90210"},
+//       {name: "Sumaira", email: "sum@gmail.com", phone: "7142222222", zip: "90001"}
+//     )
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+  // });
